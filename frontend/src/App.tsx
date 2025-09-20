9fb1103c301880da744b106fc8671f462948dade
Cyanoxide <cyanoxide@gmail.com>
2025-09-20 19:51:46 +0100

    Set up initial content boxes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,37 @@
-import ContentBox from './components/contentBox/contentBox'
+import ContentBox from './components/contentBox/ContentBox'
+import { useState, useEffect } from 'react';
 
 function App() {
+  interface PartyMember {
+    name: string;
+    level: number;
+    hp: number;
+    mp: number;
+    limit_level: number;
+    image_path: string;
+  }
 
-  fetch("http://localhost:8000/partymember/1")
-    .then(res => res.json())
-    .then(data => console.log(data));
+  const [partyMember, setPartyMember] = useState<PartyMember | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/partymember/1")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setPartyMember(data)
+      });
+  }, [])
 
   return (
     <>
-      <ContentBox>Test</ContentBox>
+      <div className="flex h-screen">
+        <div className="w-[1100px] h-[800px] m-auto relative">
+          <ContentBox className="w-[1000px] h-[750px] m-auto" top={25}>{partyMember && partyMember.name}</ContentBox>
+          <ContentBox className="w-[275px] h-[500px] m-auto" right={0}>Test</ContentBox>
+          <ContentBox className="w-[275px] h-[100px] m-auto" right={0} bottom={85}>Test</ContentBox>
+          <ContentBox className="w-[500px] h-[75px] m-auto" right={0} bottom={0}>Test</ContentBox>
+        </div>
+      </div>
     </>
   )
 }
--- frontend/src/components/contentBox/ContentBox.tsx
+++ frontend/src/components/contentBox/ContentBox.tsx
@@ -4,11 +4,17 @@ import styles from './contentBox.module.scss';
 
 interface contentBoxProps {
     children: ReactNode;
+    top?: number | "auto",
+    right?: number | "auto",
+    bottom?: number | "auto",
+    left?: number | "auto",
+    zIndex?: number,
+    className?: string,
 }
 
-const ContentBox: React.FC<contentBoxProps> = ({ children, ...props }) => {
+const ContentBox: React.FC<contentBoxProps> = ({ children, top, right, bottom, left, zIndex, className, ...props }) => {
     return (
-        <div className={styles.contentBox} {...props}>
+        <div className={`${styles.contentBox} ${className}`} style={{ top, right, bottom, left, zIndex }} {...props}>
             {children}
         </div>
     );
