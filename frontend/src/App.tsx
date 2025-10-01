c6864b708d87240b0a692210b263cdaad65bb538
Cyanoxide <cyanoxide@gmail.com>
2025-10-01 19:03:55 +0100

    Implement new components to the app and style accordingly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@
 import ContentBox from './components/contentBox/ContentBox';
 import PartyMember from './components/PartyMember/PartyMember';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import textToSprite from "./util/textToSprite";
+import Time from './components/Time/Time';
 
 function App() {
   const queryClient = new QueryClient();
@@ -9,13 +11,38 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="flex h-screen">
-        <div className="w-[1100px] h-[800px] m-auto relative">
-          <ContentBox className="w-[1000px] h-[750px] m-auto" top={25}>
-            <PartyMember memberId={1}/>
+        <div className="w-[1100px] h-[825px] m-auto relative">
+          <ContentBox className="w-[1000px] h-[720px] m-auto" top={44}> {/* Right */}
+            <PartyMember memberId={1} />
           </ContentBox>
-          <ContentBox className="w-[275px] h-[500px] m-auto" right={0}>Test</ContentBox>
-          <ContentBox className="w-[275px] h-[100px] m-auto" right={0} bottom={85}>Test</ContentBox>
-          <ContentBox className="w-[500px] h-[75px] m-auto" right={0} bottom={0}>Test</ContentBox>
+          <ContentBox className="w-[270px] h-[530px] m-auto" right={0} data-label="menu"> {/* Down */}
+            <ul>
+              <li className="h-[29px] mb-4">{textToSprite("Item")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Magic")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Materia")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Equip")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Status")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Order")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Limit")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Config")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("PHS")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Save")}</li>
+              <li className="h-[29px] mb-4">{textToSprite("Quit")}</li>
+            </ul>
+          </ContentBox>
+          <ContentBox className="w-[280px] h-[110px] m-auto" right={0} bottom={110} data-label="metaInfo">
+            <ul className="flex justify-between flex-col h-full">
+              <li className="flex justify-between">
+                <span>{textToSprite("Time")}</span>
+                <Time />
+              </li>
+              <li className="flex justify-between">
+                <span>{textToSprite("Gil")}</span>
+                <span>{textToSprite("77777", true)}</span>
+              </li>
+            </ul>
+          </ContentBox> {/* left */}
+          <ContentBox className="w-[535px] h-[95px] m-auto" right={0} bottom={0} data-label="pageInfo">{textToSprite("Sector 7 Slums")}</ContentBox> {/* Up */}
         </div>
       </div>
     </QueryClientProvider>
--- frontend/src/components/PartyMember/PartyMember.tsx
+++ frontend/src/components/PartyMember/PartyMember.tsx
@@ -1,5 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchPartyMember } from '../../util/http.ts';
+import { useQuery } from "@tanstack/react-query";
+import { fetchPartyMember } from "../../util/http.ts";
+import textToSprite from "../../util/textToSprite.tsx";
+import styles from "./PartyMember.module.scss"
+
+import ProgressBar from "../ProgressBar/ProgressBar.tsx";
+import ResourceCounter from "../ResourceCounter/ResourceCounter.tsx";
 
 interface partyMemberProps {
     memberId: number
@@ -18,14 +23,14 @@ const PartyMember: React.FC<partyMemberProps> = ({ memberId }) => {
     let content;
 
     if (isLoading) {
-        content = <p>Loading</p>;
+        content = <p>{textToSprite("Loading...")}</p>;
     }
 
     if (isError) {
         content = (
             <>
-                <p>An error occurred</p>
-                <p>{error.message}</p>
+                <p>{textToSprite("An error occurred")}</p>
+                <p>{textToSprite(error.message)}</p>
             </>
         );
     }
@@ -34,17 +39,26 @@ const PartyMember: React.FC<partyMemberProps> = ({ memberId }) => {
         const { name: memberName, level, hp, mp, limit_level, image_path } = data;
 
         content = (
-            <div className="flex">
-                <img src={image_path} alt="Party Member Portrait" />
-                <div>
-                    <p>{memberName}</p>
-                    <p>{level}</p>
-                    <p>{hp}</p>
-                    <p>{mp}</p>
+            <div className={`${styles.partyMember} flex justify-between`}>
+                <img src={image_path} alt="Party Member Portrait" width={145} className="object-contain" />
+                <div className="mt-2 ml-8">
+                    <p className="mb-2">{textToSprite(memberName)}</p>
+                    <p className="flex">
+                        <span className="font-glyph" data-sprite="lv">lv</span>
+                        {textToSprite(level.toString(), true)}
+                    </p>
+                    <ResourceCounter label="hp" maxValue={hp} currentValue={hp} accentColor="#4f8fd4" />
+                    <ResourceCounter label="mp" maxValue={mp} currentValue={mp} accentColor="#63d9c1" />
                 </div>
-                <div>
-                    <p>{level}</p>
-                    <p>{limit_level}</p>
+                <div className="mt-12">
+                    <p>{textToSprite("next level")}</p>
+                    <div className="ml-7">
+                        <ProgressBar percentage={50} />
+                    </div>
+                    <p>{textToSprite(`Limit level ${limit_level.toString()}`)}</p>
+                    <div className="ml-7">
+                        <ProgressBar percentage={100} accentColor="#dfbddd" data-limit="true" />
+                    </div>
                 </div>
             </div>
         );
