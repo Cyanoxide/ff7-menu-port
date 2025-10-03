3cf422ea1b0a75a72aa5019a2280110a2934a224
Cyanoxide <cyanoxide@gmail.com>
2025-10-03 17:55:55 +0100

    Misc. Changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,45 +4,53 @@ import PartyMember from './components/PartyMember/PartyMember';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import textToSprite from "./util/textToSprite";
 import Time from './components/Time/Time';
+import Menu from './components/Menu/Menu';
+import { useEffect, useState } from 'react';
 
 function App() {
   const queryClient = new QueryClient();
 
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [activePage, setActivePage] = useState("home");
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex h-screen">
+      <div className="flex h-screen" data-active={isLoaded}>
         <div className="w-[1100px] h-[825px] m-auto relative">
-          <ContentBox className="w-[1000px] h-[720px] m-auto" top={44}> {/* Right */}
-            <PartyMember memberId={1} />
-          </ContentBox>
-          <ContentBox className="w-[270px] h-[530px] m-auto" right={0} data-label="menu"> {/* Down */}
-            <ul>
-              <li className="h-[29px] mb-4">{textToSprite("Item")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Magic")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Materia")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Equip")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Status")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Order")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Limit")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Config")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("PHS")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Save")}</li>
-              <li className="h-[29px] mb-4">{textToSprite("Quit")}</li>
-            </ul>
-          </ContentBox>
-          <ContentBox className="w-[280px] h-[110px] m-auto" right={0} bottom={110} data-label="metaInfo">
-            <ul className="flex justify-between flex-col h-full">
-              <li className="flex justify-between">
-                <span>{textToSprite("Time")}</span>
-                <Time />
-              </li>
-              <li className="flex justify-between">
-                <span>{textToSprite("Gil")}</span>
-                <span>{textToSprite("77777", true)}</span>
-              </li>
-            </ul>
-          </ContentBox> {/* left */}
-          <ContentBox className="w-[535px] h-[95px] m-auto" right={0} bottom={0} data-label="pageInfo">{textToSprite("Sector 7 Slums")}</ContentBox> {/* Up */}
+          {activePage === "home" &&
+            <>
+              <ContentBox className="w-[1000px] h-[720px] m-auto" top={44} data-label="party">
+                <PartyMember memberId={1} />
+                <ContentBox left={53} bottom={79} right={220} top={303} data-label="bio">
+                  <p className="mb-2">{textToSprite("I'm a Senior Web Developer based in")}</p>
+                  <p className="mb-6">{textToSprite("Gloucester, UK.")}</p>
+                  <p className="mb-6">{textToSprite("Welcome to my WIP sandbox.")}</p>
+                  <p className="mb-2">{textToSprite("I plan to add an ever-growing collection")}</p>
+                  <p className="mb-2">{textToSprite("of small technical projects here, mostly")}</p>
+                  <p className="mb-2">{textToSprite("built with PS1 aesthetics in mind.")}</p>
+                </ContentBox>
+              </ContentBox>
+              <ContentBox className="w-[280px] h-[110px] m-auto" right={0} bottom={110} data-label="metaInfo">
+                <ul className="flex justify-between flex-col h-full">
+                  <li className="flex justify-between">
+                    <span>{textToSprite("Time")}</span>
+                    <Time />
+                  </li>
+                  <li className="flex justify-between">
+                    <span>{textToSprite("Gil")}</span>
+                    <span>{textToSprite("77777", true)}</span>
+                  </li>
+                </ul>
+              </ContentBox>
+              <ContentBox className="w-[535px] h-[95px] m-auto" right={0} bottom={0} data-label="pageInfo">{textToSprite("Homepage")}</ContentBox>
+            </>
+          }
+          {activePage !== "home" && <ContentBox data-label="test">{textToSprite("Test")}</ContentBox>}
+          <Menu activePage={activePage} setActivePage={setActivePage} />
         </div>
       </div>
     </QueryClientProvider>
