75b8cff76f8cb0ec6f3be9563ba0359dcbd0d0fd
Cyanoxide <cyanoxide@gmail.com>
2025-10-05 14:32:12 +0100

    Add support for highlighted text color

--- frontend/src/util/textToSprite.tsx
+++ frontend/src/util/textToSprite.tsx
@@ -1,8 +1,8 @@
-const textToSprite = (text: string, isResourceValue: boolean = false) => {
+const textToSprite = (text: string, isResourceValue: boolean = false, isHighlight = false) => {
     if (!text) return;
 
     return (
-        <span className={`font flex`} data-label={(isResourceValue) ? "resourceValue" : null}>
+        <span className={`font flex`} data-label={(isResourceValue) ? "resourceValue" : null} data-highlighted={isHighlight ? true : null}>
             {text.split("").map((glyph: string, index: number) => (
                 <span key={index} className="font-glyph" data-sprite={glyph}>{glyph}</span>
             ))}
