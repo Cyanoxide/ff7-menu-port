8aad143c0bd09dfd6e6d8df0fc3e85e0f8984394
Cyanoxide <cyanoxide@gmail.com>
2025-10-06 12:57:13 +0100

    Add initial HistoryContent component

--- frontend/src/components/HistoryContent/HistoryContent.tsx
+++ frontend/src/components/HistoryContent/HistoryContent.tsx
@@ -2,33 +2,37 @@ import ContentBox from "../contentBox/ContentBox";
 import HistorySave from "../HistorySave/HistorySave";
 import textToSprite from "../../util/textToSprite";
 
-import styles from "./HistoryContent.module.scss";
-
 function HistoryContent() {
     const history = [
         {
             id: 1,
             name: "Direct Online Services",
-            link: "https://www.worktop-express.co.uk/",
-            level: "24",
-            time: "59:23:01",
-            gil: 54799
+            link: "https://www.directonlineservices.com/",
+            user: "Jamie Pates",
+            level: "34",
+            role: "Senior Web Dev.",
+            year: "2020-24",
+            image_path: "/history__dos.png"
         },
         {
             id: 2,
             name: "Ruroc",
             link: "https://www.ruroc.com/",
+            user: "Jamie Pates",
             level: "28",
-            time: "34:42:30",
-            gil: 41045
+            role: "Head Web Dev.",
+            year: "2014-19",
+            image_path: "/history__ruroc.png"
         },
         {
             id: 3,
             name: "Tangy Media",
             link: "https://www.tangymedia.co.uk/",
+            user: "Jamie Pates",
             level: "22",
-            time: "8:09:49",
-            gil: 24350
+            role: "WordPress Dev.",
+            year: "2012-13",
+            image_path: "/history__tangymedia.png"
         },
     ]
 
@@ -36,11 +40,11 @@ function HistoryContent() {
         <>
             <div className="relative h-[84px] mb-[10px]">
                 <ContentBox data-label="historyHeader" className="h-full absolute top-0 left-0 right-0">{textToSprite("Select a file.")}</ContentBox>
-                <ContentBox data-label="historyFileLabel" className="h-full w-[225px] absolute top-0 right-[278px]">{textToSprite("FILE  01")}</ContentBox>
+                <ContentBox data-label="historyFileLabel" className="h-full w-[225px] absolute top-0 right-[280px]">{textToSprite("FILE  01")}</ContentBox>
             </div>
-            <HistorySave />
-            <HistorySave />
-            <HistorySave />
+            {history.map((item) => (
+                <HistorySave key={item.id} historyItem={item} />
+            ))}
         </>
     );
 }
