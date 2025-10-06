a7cf2db7074c4c097e58ec9b8b79bd75f4183bb0
Cyanoxide <cyanoxide@gmail.com>
2025-10-06 12:57:35 +0100

    Add initial HistorySave component

--- frontend/src/components/HistorySave/HistorySave.tsx
+++ frontend/src/components/HistorySave/HistorySave.tsx
@@ -1,17 +1,55 @@
 
 import ContentBox from "../contentBox/ContentBox";
-import PartyMember from "../PartyMember/PartyMember";
 import textToSprite from "../../util/textToSprite";
 
-// import styles from "./HistorySave.module.scss";
+import styles from "./HistorySave.module.scss";
 
-function HistorySave() {
+type HistoryItem = {
+    id: number;
+    name: string;
+    link: string;
+    role: string;
+    user: string;
+    year: string;
+    level: string;
+    image_path: string;
+};
+
+interface historySaveProps {
+    historyItem: HistoryItem;
+};
+
+
+const HistorySave: React.FC<historySaveProps> = ({ historyItem, ...props }) => {
+    if (!historyItem) return;
+
     return (
         <>
-            <ContentBox data-label="historySave" className="h-[235px] relative">
-                <ContentBox data-label="historySave" className="absolute w-[17rem] h-[7rem] top-[31px] right-[-2px]"></ContentBox>
-                <ContentBox data-label="historySave" className="absolute w-[34rem] h-[5rem] bottom-[-11px] right-[-2px]"></ContentBox>
-            </ContentBox>
+            <a href={historyItem.link} title={historyItem.name} target="_blank" className={`${styles.historySave} cursor-pointer`} {...props}>
+                <ContentBox data-label="historySave" className="h-[235px] relative">
+                    <div className="mr-[414px] flex gap-5">
+                        <img className="h-[11.5rem] w-auto" src={historyItem.image_path} />
+                        <img className="h-[11.5rem] w-auto" src="/portrait.png" />
+                        <div className="ml-2 mt-[1.3rem]">
+                            <p className="mb-4">{textToSprite(historyItem.user)}</p>
+                            <p className="flex"><span className="font-glyph" data-sprite="lv">lv</span><span>{textToSprite(historyItem.level, true)}</span></p>
+                        </div>
+                    </div>
+                    <ContentBox data-label="historySaveMeta" className="absolute w-[27rem] h-[7rem] top-[31px] right-[-2px]">
+                        <ul>
+                            <li className="flex justify-between mb-3">
+                                <span>{textToSprite("Role")}</span>
+                                <span>{textToSprite(historyItem.role)}</span>
+                            </li>
+                            <li className="flex justify-between">
+                                <span>{textToSprite("Years")}</span>
+                                <span>{textToSprite(historyItem.year)}</span>
+                            </li>
+                        </ul>
+                    </ContentBox>
+                    <ContentBox data-label="historySave" className="absolute w-[43.8rem] h-[5rem] bottom-[-11px] right-[-2px]">{textToSprite(historyItem.name)}</ContentBox>
+                </ContentBox>
+            </a>
         </>
     );
 }
