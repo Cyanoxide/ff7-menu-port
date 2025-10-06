56ebbb2e4db7939ae539640f5787b776d54860a3
Cyanoxide <cyanoxide@gmail.com>
2025-10-06 12:58:24 +0100

    Update menu items

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -9,16 +9,30 @@ interface MenuProps {
 
 const Menu = ({ activePage, setActivePage }: MenuProps) => {
     const menuItems: Record<string, string> = {
-        "skills": "Skills",
         "projects": "Projects",
+        "skills": "Skills",
         "history": "History",
+        "": "",
         // "config": "Config",
-        // "resume": "Resume",
         // "about": "About",
         // "contact": "Contact",
-        // "github": "Github",
-        // "donate": "Donate",
         // "quit": "Quit"
+    }
+
+    const menuLinks: Record<string, { name: string; path: string; download?: boolean }> = {
+        "resume": {
+            name: "Résumé",
+            path: "/Jamie_Pates_Resume_2025.pdf",
+        },
+        "github": {
+            name: "Github",
+            path: "https://github.com/Cyanoxide",
+        },
+        "donate": {
+            name: "Donate",
+            path: "https://ko-fi.com/cyanoxide",
+
+        }
     }
 
     const handleClose = () => {
@@ -26,7 +40,7 @@ const Menu = ({ activePage, setActivePage }: MenuProps) => {
     }
 
     return (
-        <ContentBox className={`m-auto w-[270px] absolute right-0 ${(activePage !== "home") ? "h-[84px]" : "h-[530px]"}`} data-label="menu">
+        <ContentBox className={`m-auto w-[270px] absolute right-0 ${(activePage !== "home") ? "h-[84px]" : "h-[530px]"}`} data-label="menu" >
             <ul className={styles.menu}>
                 {Object.keys(menuItems).map((menuItem) => (
                     <li key={menuItem} className={`${["home", menuItem].includes(activePage) ? "h-[29px] mb-4" : "h-0 invisible"} flex justify-between`}>
@@ -34,8 +48,13 @@ const Menu = ({ activePage, setActivePage }: MenuProps) => {
                         {activePage !== "home" && <ContentBox className="absolute" data-label="close"><span onClick={handleClose}>{textToSprite("X")}</span></ContentBox>}
                     </li>
                 ))}
+                {Object.keys(menuLinks).map((menuItem) => (
+                    <li key={menuItem} className={`${["home", menuItem].includes(activePage) ? "h-[29px] mb-4" : "h-0 invisible"} h-[29px] mb-4 flex justify-between`}>
+                        <a title={menuLinks[menuItem].name} href={menuLinks[menuItem].path} target="_blank">{textToSprite(menuLinks[menuItem].name)}</a>
+                    </li>
+                ))}
             </ul>
-        </ContentBox>
+        </ContentBox >
     );
 }
 
